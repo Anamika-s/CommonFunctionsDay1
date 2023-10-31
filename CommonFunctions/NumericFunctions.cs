fd0441f9356c17920ea175166a4bbc6543611e10
Anamika-s <anamika.sawhney22@gmail.com>
2023-10-31 17:04:51 +0530

    Add project files.

---
+++ CommonFunctions/NumericFunctions.cs
@@ -0,0 +1,25 @@
+﻿namespace CommonFunctions
+{
+    public static class NumericFunctions
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static bool IsEven(int x)
+        {
+            //if (x % 2 == 2) return true;
+            //else 
+            //    return false;
+            //?:
+            return x%2==0?true : false;
+        }
+
+    }
+}
---
+++ CommonFunctions/StringFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonFunctions
+{
+    public class StringFunctions
+    {
+        //System.String string
+        //int System.Int32
+       
+        public String StringCombine(string sent1, string sent2)
+        {
+            return string.Concat(sent1, " ", sent2);
+        }
+        public string Table(int num)
+        {
+             StringBuilder sb = new StringBuilder();
+             
+            for(int x= 1; x <= 10; x++)
+            {
+                sb.AppendLine($"{num} * {x} = {num * x}");
+            }
+            return sb.ToString();
+        }
+
+    }
+}
